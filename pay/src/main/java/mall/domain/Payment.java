96b30ae829c97ba06074ec9370888df088f327c0
fullcrescent <fullcrescent@naver.com>
2022-12-06 06:00:42 +0000

    서킷 브레이커 시간 설정 및 과부하 테스트 완료

--- pay/src/main/java/mall/domain/Payment.java
+++ pay/src/main/java/mall/domain/Payment.java
@@ -71,7 +71,7 @@ public class Payment  {
             });
 
             try {
-                Thread.currentThread().sleep((long) (1000 + Math.random() * 220));
+                Thread.currentThread().sleep((long) (400 + Math.random() * 220));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
